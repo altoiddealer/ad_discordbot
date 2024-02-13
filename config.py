7796437f98143f67c209412316c9b6f71cdb7f04
altoiddealer <altoiddealer@gmail.com>
2024-02-13 14:49:47 -0500

    Major Update: Tags Feature
    First there was a basic "presets" feature.  Then there was "ImgLoras", which became "ImgTags". Meanwhile, settings for new features have been piling up in config.py.
    
    Today I unveil a highly flexible and versatile 'Tags' system, which is a simple way to control most of the bot features while adding an extra layer of customization - the ability to have their effects apply globally, for specific imgmodels, or for specific characters.
    
    With the tags system, there are seemingly limitless possibilities when it comes to triggering: modifying user prompt/payload to the LLM, LLM's prompt/payload for A1111, swapping characters, activating extensions, face swapping, censoring, and so on.

--- config.py
+++ config.py
@@ -27,46 +27,25 @@ discord = {
 
 sd = {
     'A1111': "http://127.0.0.1:7860",   # Default URL for A1111 API. Adjust if you have issues connecting.
-    # Bot has integrations with the following extensions. **Only enable them if they are installed AND active in A1111.**
+    # Bot has integrations with the following extensions. **Only enable them if they are installed AND enabled in A1111.**
     'extensions': {
-        'controlnet_enabled': False,    # Requires: sd-webui-controlnet AND configuring ad_discordbot/dict_cmdoptions.yaml
-        'reactor_enabled': False,        # Requires: sd-webui-reactor
-        'lrctl': {                     # Requires: sd-webui-loractl (https://github.com/cheald/sd-webui-loractl)
+        'controlnet_enabled': False,    # Requires: sd-webui-controlnet (https://github.com/Mikubill/sd-webui-controlnet) AND configuring 'dict_cmdoptions.yaml'
+        'reactor_enabled': False,       # Requires: sd-webui-reactor (https://github.com/Gourieff/sd-webui-reactor) AND configuring 'dict_faces.yaml'
+        'lrctl': {                      # Requires: sd-webui-loractl (https://github.com/cheald/sd-webui-loractl)
             'enabled': False,   # This extension enhances LORA processing by allowing adjustable weights during image generation. Weight is scaled linearly between step definitions.
             'min_loras': 2,     # Minimum number of loras in the prompt to trigger the following weight scaling definitions.
-            'lora_1_scaling': '1.0@0,0.5@0.5',  # format is (% of existing lora weight)@(step percent),(more step definitions). These are not static weights- these are multiplied by the existing LORA weight. '1.0@0' = begin generation with existing LORA weight.
-            'lora_2_scaling': '0.5@0,1.0@0.5',  # You can define as many steps as you want. ex1: '0.5@0' would just cut the LORA weight in half. ex2: '0.0@0,1.0@0.5,0.0@1.0' would look like a pyramid on the graph.
+            'lora_1_scaling': '1.0@0,0.5@0.4',  # format is (% of existing lora weight)@(step percent),(more step definitions). These are not static weights- these are multiplied by the existing LORA weight. '1.0@0' = begin generation with existing LORA weight.
+            'lora_2_scaling': '0.5@0,1.0@0.4',  # You can define as many steps as you want. ex1: '0.5@0' would just cut the LORA weight in half. ex2: '0.0@0,1.0@0.5,0.0@1.0' would look like a pyramid on the graph.
             # More scaling definitions can be added ex: 'lora_3_scaling' etc
             'additional_loras_scaling': '0.4@0.0,0.8@0.5'   # Applies to LORAs when there are more than specifically defined. If 4 loras are found in prompt, but only defined up to lora_2_scaling, then the last 2 LORAs would be scaled by this definition.
         }
-    },
-    # Feature to compare the image prompt to defined trigger phrases and determine if image should be censored
-    'nsfw_censoring': {
-        'enabled': False,
-        'mode': 0,   # 0 = blur image / 1 = block image from being generated
-        'triggers': ['nude', 'erotic']
-    },
-    # Feature to add randomness to payload parameters. For each image request, a random value from each range will be merged with your current settings (activesettings.yaml).
-    'param_variances': {
-        'enabled': False,
-        'presets': [
-            {'cfg_scale': (-2,2), 'steps': (0,10), 'hr_scale': (-0.15,0.15)}
-        ]
-    } 
+    }
 }
 
 # Former default behavior of textgen-webui (but not anymore). Use this setting to update 'context' + 'greeting'.
 replace_char_names = {
-    'replace_user': '{{user}}', # replace every instance with user's username. '' = don't replace anything
-    'replace_char': '{{char}}'   # replace every instance with bot's character name. '' = don't replace anything
-}
-
-# Slips in a message about the current time before your context.
-tell_bot_time = {
-    'enabled': True,
-    'mode': 0,                      # 0 = text requests only / 1 = image requests only / 2 = both
-    'message': "It is now {}\n",    # datetime is inserted at {}
-    'time_offset': 0.0 # 0 = today's date (system time). -0.5 shifts the current date to be 12 hours ago. 100000 sets the date to be 100000 days in the future.
+    'replace_user': '{{user}}', # replace every instance with user's username
+    'replace_char': '{{char}}'   # replace every instance with bot's character name
 }
 
 # Imgmodels can be switched by using /imgmodels command, or enabling 'auto_change_models' (or manually updating activesettings.yaml)
@@ -78,27 +57,25 @@ imgmodels = {
         'presets': [                # Defininitions for if 'guess_model_res' = True.  Add presets as desired, sorted in ascending order.
             {'max_filesize': 6.0,                               # 'max_filesize' expressed in GB.
                 'width': 512, 'height': 512, 'enable_hr': True, # Any defined 'payload' options will be updated.
-                'imgtag_name': 'SD15 ImgTags'},                  # If you specify an imgtag_name from 'dict_imgtags.yaml', those will also be swapped in.
+                'tag_preset_name': 'SD15 Tags'},                # If you specify a tag_preset_name from 'dict_tags.yaml', those will also be swapped in.
             {'max_filesize': 100.0,
                 'width': 1024, 'height': 1024, 'enable_hr': False, 
-                'imgtag_name': 'SDXL ImgTags'}
+                'tag_preset_name': 'SDXL Tags'}
         ]
     },
 
     # Omit models which include matching text.
-    'exclude': ['inpaint', 'refiner'],
+    'exclude': ['inpaint', 'refiner', 'base'],
     # Only consider models which include matching text. '' = No filtering (loads all models)
     'filter': [''], # Example: 'xl' = likely just SDXL models. Can match multiple such as ['xl', '15']
 
     # Feature to periodically switch imgmodels. Behavior is affected by setting for 'get_imgmodels_via_api'
-    'auto_change_models': {
+    'auto_change_imgmodels': {
         'enabled': False,
         'mode': 'random',       # 'random' = picks model at random / 'cycle' = sequential order
-        'frequency': 1.0,       # How often to change models, in hours. 0.5 = 30 minutes
+        'frequency': 2.0,       # How often to change models in hours. 0.5 = 30 minutes
         'channel_announce': ''  # If a channel is specified, it will announce/update as configured below. '' = Don't announce/update topic.
     },
-
-    # Options to update topic / announce new img model. When using /imgmodel, it will use the channel where the command was executed.
     'update_topic': {
         'enabled': True,
         'topic_prefix': "**Current Image Model:** ",
@@ -110,74 +87,4 @@ imgmodels = {
         'include_url': True,        # URL only applies if 'get_imgmodels_via_api': False
         'include_params': False     # If True, lists all the current A1111 parameters
     }
-}
-
-# Feature to modify settings / prompts from trigger phrases
-imgprompt_settings = {
-    'trigger_search_mode': 'userllm',   # What to compare triggers against. 'user' = user prompt only / 'llm' = bot reply only / 'userllm' = search all text
-    'insert_imgtags_in_prompt': True,     # ImgTag handling. True = insert positive_prompt after matches found in prompt / False = append all to end of prompt.
-
-    # Trigger an image response with words/phrases.
-    'trigger_img_gen_by_phrase': {
-        'enabled': True,        # If you want phrases removed from your prompt, use dynamic_context configuration for that.
-        'on_prefix_only': True, # if True, image response only occurs when prompt begins with trigger phrase.
-        'triggers': ['draw', 'generate an image', 'generate a picture', 'generate a photo', 'take a photo', 'take a picture', 'take another picture', 'take a selfie', 'take another selfie', 'take a self portrait']
-    },
-
-    # Modifies payload settings if prompt includes trigger phrases
-    'trigger_img_params_by_phrase': {
-        'enabled': True,
-        'presets': [
-            {'triggers': ['vertical', 'selfie', 'self portrait'],
-                'width': 896, 'height': 1152},
-            {'triggers': ['landscape'],
-                'width': 1152, 'height': 896}
-        ]
-    },
-
-    # Modifies payload settings if prompt includes trigger phrases
-    'trigger_faces_by_phrase': {
-        'enabled': True,
-        'presets': [
-            {'triggers': ['alfred neuman', 'mad magazine guy'],
-                'face_swap': 'neuman.png'}, # face_swap can be used for Reactor extension. Valid file types: .png, .jpg, .txt (containing base64 string)
-            {'triggers': ['donald trump'],
-                'face_swap': 'trump.txt'}
-        ]
-    }
-}
-
-# Feature to swap in custom character/settings when your prompt includes pre-defined trigger phrases
-dynamic_context = {
-    'enabled': True,
-    'print_results': True, # Whether to print results to console
-    # Define as many presets as you want (copy/paste, edit).
-    # If multiple presets are matched in user's prompt, the highest one on this list will be prioritized.
-    # Similarly for "remove_trigger_phrase", phrases are removed from left to right (put your verbose triggers first)
-    'presets': [
-        {'triggers': ['draw', 'generate'],
-            'on_prefix_only': True,             # Only trigger if user prompt begins with the phrase.
-            'swap_character': 'M1nty-SDXL',   # filename of character in /characters. Character's llmstate params will be included. '' = don't swap character.
-            'remove_trigger_phrase': True,      # if True, removes the phrase from your prompt so the LLM does not see it
-            'load_history': -1,     # 0 = default (all history included) / -1 = prompt excludes chat history / > 1 = llm only sees this many recent exchanges.
-            'save_history': False,  # whether to save this interaction to history or not.
-            'add_instruct': '{}'    # adds an instruction. {} is where your prompt is inserted.
-        },
-        {'triggers': ['take a photo', 'take a picture', 'take another picture'],
-            'on_prefix_only': False,
-            'swap_character': '',
-            'remove_trigger_phrase': False,
-            'load_history': 0,
-            'save_history': True,
-            'add_instruct': '[SYSTEM] You have been tasked with generating an image: "{}". Describe the image in vivid detail as if you were describing it to a blind person. The description in your response will be sent to an image generation API.'
-        },
-        {'triggers': ['selfie', 'self portrait'],
-            'on_prefix_only': False,
-            'swap_character': '',
-            'remove_trigger_phrase': False,
-            'load_history': 0,
-            'save_history': True,
-            'add_instruct': '[SYSTEM] You have been tasked with taking a selfie: "{}". Include your appearance, your current state of clothing, your surroundings and what you are doing right now.'
-        }
-    ]
 }
