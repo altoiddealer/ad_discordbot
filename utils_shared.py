146b66f324c1c4ca5f7504970f4cd5798c2beeb9
Artificiangel <cca@aquilatech.com>
2024-05-15 23:32:43 -0400

    move starboard back, add migration

--- utils_shared.py
+++ utils_shared.py
@@ -1,15 +1,16 @@
 import asyncio
 import os
+import logging
 
 task_semaphore = asyncio.Semaphore(1)
 
 class SharedPath:
     dir_root = 'ad_discordbot'
     dir_internal = os.path.join(dir_root, 'internal')
+    os.makedirs(dir_internal, exist_ok=True)
 
     # Internal
     active_settings = os.path.join(dir_internal, 'activesettings.yaml')
-    starboard = os.path.join(dir_internal, 'starboard_messages.yaml')
 
     # Configs
     config = os.path.join(dir_root, 'config.yaml')
@@ -17,5 +18,14 @@ class SharedPath:
     cmd_options = os.path.join(dir_root, 'dict_cmdoptions.yaml')
     img_models = os.path.join(dir_root, 'dict_imgmodels.yaml')
     tags = os.path.join(dir_root, 'dict_tags.yaml')
+    starboard = os.path.join(dir_root, 'starboard_messages.yaml')
 
 shared_path = SharedPath()
+
+
+
+_old_active = os.path.join(shared_path.dir_root, 'activesettings.yaml')
+if os.path.isfile(_old_active):
+    logging.info(f'Migrating file to {shared_path.active_settings!r}')
+    os.rename(_old_active, shared_path.active_settings)
+    
