ecaba8cdc301700f7f5aa1c8a9e6fcc46e918149
Artificiangel <cca@aquilatech.com>
2024-05-28 15:50:32 -0400

    add template script for future

---
+++ script.py
@@ -0,0 +1,12 @@
+# Does nothing at the moment
+params = {
+    "display_name": "AD Discord Bot",
+    "is_tab": False,
+}
+
+
+def setup():
+    """
+    Gets executed only once, when the extension is imported.
+    """
+    pass
