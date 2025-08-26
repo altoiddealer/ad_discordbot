0976fa7a553dce095fe02d590c2e032d2b26a8af
marcos33998 <129631532+marcos33998@users.noreply.github.com>
2025-08-26 11:46:21 +0200

    Cleaning

--- config_stt.py
+++ config_stt.py
@@ -1,8 +1,6 @@
 # config.py
-# Replace with your actual Discord bot token and text channel ID where transcriptions should be sent.
-TOKEN = ""
 
-WRAP_MESSAGES = False # Set to True to wrap messages, False for individual messages
+WRAP_MESSAGES = True # Set to True to wrap messages, False for individual messages
 
 ASR_ENGINE = "whisper"  # "whisper", "omnisense" or "faster_whisper"
 
@@ -19,7 +17,7 @@ OMNISENSE_QUANTIZE = True  # Use quantized model
 OMNISENSE_MODEL_DIR = "SenseVoiceSmall"  # None for default
 
 # Faster-Whisper configuration
-FASTER_WHISPER_MODEL_SIZE = "base"  # Should be same as for Whisper, check faster_whisper documentation
+FASTER_WHISPER_MODEL_SIZE = "small"  # Should be same as for Whisper, check faster_whisper documentation
 FASTER_WHISPER_DEVICE = "cuda"  # I'll let you use cpu this time,"cuda" or "cpu"
 FASTER_WHISPER_COMPUTE_TYPE = "int8_float16"  # Compute type: "float16" (GPU), "int8_float16" (GPU), "int8" (CPU)
 
@@ -28,12 +26,12 @@ class AudioConfig:
     SPEECH_VOLUME_THRESHOLD = 1 # Not recommended to change
     CHUNK_SILENCE_THRESHOLD = 0.5  # Seconds to process a chunk
     FINAL_SILENCE_THRESHOLD = 0.8  # Seconds to finalize transcription
-    SAMPLE_RATE = 48000
-    CHANNELS = 2
-    SAMPLE_WIDTH = 2  # 16-bit PCM
+    SAMPLE_RATE = 48000 # Discord's default
+    CHANNELS = 1 # Discord's default
+    SAMPLE_WIDTH = 2  # Discord's default
     
     # Volume normalization parameters, adjust if needed
     TARGET_RMS = 1400
     MAX_GAIN = 10
-
     MIN_RMS = 100
+
